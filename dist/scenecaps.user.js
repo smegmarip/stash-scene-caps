a4a38ec25d42027fafb36cf8dd1ce007b4fb8a40
Smegmarip <smegmarip@gmail.com>
2023-12-23 19:56:07 -0500

    Annotate sprite with existing markers.

--- dist/scenecaps.user.js
+++ dist/scenecaps.user.js
@@ -2,7 +2,7 @@
 // @name        scenecaps
 // @description Toggle Screen Caps on Scene Player
 // @namespace   https://github.com/smegmarip
-// @version     0.0.3
+// @version     0.0.5
 // @homepage    https://github.com/smegmarip/stash-scene-caps/
 // @author      smegmarip
 // @match       http://localhost:9999/*
@@ -63,7 +63,7 @@
                               </button>
                           </div>
                         </div>
-                    </div>      		
+                    </div>
                   </div>
                 </div>
                 <div class="modal-body">
@@ -82,6 +82,10 @@
           </div>
           `,
         },
+      },
+      icons: {
+        clapper: `<svg width="16" height="16" fill="#FFFFFF" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 402.598 402.598" xml:space="preserve"><g id="XMLID_42_"><rect id="XMLID_48_" x="192.93" y="272.598" width="50" height="40"/><path id="XMLID_43_" d="M148.53,182.598l205.016-91.236L312.888,0L29.667,126.039l33.263,74.745v201.813h310v-220H148.53zM136.685,111.25l38.762-17.249l-20.281,52.808l-38.762,17.25L136.685,111.25z M228.046,70.593l38.761-17.25l-20.28,52.808L207.765,123.4L228.046,70.593z M122.93,272.598v-30h190v30h-40v40h40v30h-190v-30h40v-40H122.93z"/></g></svg>`,
+        marker: `<svg xmlns="http://www.w3.org/2000/svg" style="opacity: 0.9;" height="50%" fill="#FD7E14" viewBox="0 0 384 512"><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>`,
       },
     },
   };
@@ -135,6 +139,25 @@
     };
     var result = await stash$1.callGQL(reqData);
     return result.data.findScene.tags.map((p) => p.id);
+  }
+
+  async function getSceneMarkers(scene_id) {
+    const reqData = {
+      variables: {
+        scene_id,
+      },
+      query: `
+        query FindSceneMarkers($scene_id: ID) {
+          findScene(id: $scene_id) {
+            scene_markers {
+              id
+              seconds
+            }
+          }
+        }`,
+    };
+    let result = await stash.callGQL(reqData);
+    return result.data?.findScene?.scene_markers;
   }
 
   /**
@@ -443,7 +466,7 @@
     img.src = url;
   }
 
-  async function getTimeOffset(spriteUrl, spritePos, mouseCoords) {
+  async function getSelectedFrame(spriteUrl, spritePos, mouseCoords) {
     // get vtt blob
     const vtt_url = spriteUrl.replace("_sprite.jpg", "_thumbs.vtt");
     let vtt = await download(vtt_url);
@@ -466,7 +489,7 @@
       clickFrame.spriteUrl = spriteUrl;
     }
 
-    displayModal(clickFrame);
+    return clickFrame;
   }
 
   function close_modal() {
@@ -544,6 +567,45 @@
     });
   }
 
+  async function annotateSprite(spriteUrl) {
+    const [_, scene_id] = getScenarioAndID();
+    const sceneMarkers = await getSceneMarkers(scene_id);
+    const screenCaps = document.querySelector("#screencaps");
+    const link = screenCaps.querySelector("a");
+    if (sceneMarkers) {
+      let icon = ui.templates.icons.marker,
+        vtt_url = spriteUrl.replace("_sprite.jpg", "_thumbs.vtt"),
+        vtt = await download(vtt_url),
+        $markerEl,
+        marker,
+        f,
+        off;
+      bgImageSize(screenCaps, spriteUrl, (spritePos) => {
+        const frameData = getVTTFrames(vtt, spritePos.scale);
+        for (marker of sceneMarkers) {
+          f = frameData.reduce((s, o) => {
+            s = Math.abs(marker.seconds - o.time) < 1.0 ? o : s;
+            return s;
+          }, null);
+          if (f) {
+            off = ["left", "top", "right", "bottom"].reduce((o, k) => {
+              if (["left", "right"].includes(k)) {
+                o[k] += spritePos.position.x;
+              } else {
+                o[k] += spritePos.position.y;
+              }
+              return o;
+            }, f.offset);
+            $markerEl = $(
+              `<div class="screen-marker" style="display: flex; justify-content: center; align-items: center; position: absolute; left: ${off.left}px; top: ${off.top}px; width: ${off.width}px; height: ${off.height}px; z-index: 2; pointer-events: none;">${icon}</div>`
+            );
+            link.appendChild($markerEl.get(0));
+          }
+        }
+      });
+    }
+  }
+
   const { stash } = unsafeWindow.stash;
 
   stash.addEventListener("page:scene", function () {
@@ -556,6 +618,7 @@
         if (!document.querySelector("#screencaps")) {
           const screenCaps = document.createElement("div");
           const link = document.createElement("a");
+
           screenCaps.setAttribute("id", "screencaps");
           screenCaps.setAttribute(
             "style",
@@ -572,12 +635,16 @@
           // link.setAttribute("target", "_screencap");
           link.setAttribute(
             "style",
-            "display: block; background:transparent; position: absolute; top: 10px; left: 10px; bottom: 10px; right: 10px; cursor: pointer;"
+            "display: block; background:transparent; position: absolute; top: 0px; left: 0px; bottom: 0px; right: 0px; cursor: pointer; z-index: 3;"
           );
           link.addEventListener("click", function (e) {
             const mouseCoords = { x: e.offsetX, y: e.offsetY };
             bgImageSize(screenCaps, spriteUrl, (spritePos) => {
-              getTimeOffset(spriteUrl, spritePos, mouseCoords);
+              getSelectedFrame(spriteUrl, spritePos, mouseCoords).then(
+                (frame) => {
+                  displayModal(frame);
+                }
+              );
             });
           });
           screenCaps.appendChild(link);
@@ -588,13 +655,16 @@
               btn.setAttribute("id", "scenecaps");
               btn.setAttribute("class", "minimal pr-1 btn btn-secondary");
               const spn = document.createElement("span");
-              const svg = `<svg width="16" height="16" fill="#FFFFFF" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 402.598 402.598" xml:space="preserve"><g id="XMLID_42_"><rect id="XMLID_48_" x="192.93" y="272.598" width="50" height="40"/><path id="XMLID_43_" d="M148.53,182.598l205.016-91.236L312.888,0L29.667,126.039l33.263,74.745v201.813h310v-220H148.53zM136.685,111.25l38.762-17.249l-20.281,52.808l-38.762,17.25L136.685,111.25z M228.046,70.593l38.761-17.25l-20.28,52.808L207.765,123.4L228.046,70.593z M122.93,272.598v-30h190v30h-40v40h40v30h-190v-30h40v-40H122.93z"/></g></svg>`;
+              const svg = ui.templates.icons.clapper;
               spn.innerHTML = svg;
               btn.appendChild(spn);
               $btnGrpEl.prepend(btn);
               btn.addEventListener("click", function () {
                 if (screenCaps.style.display === "none") {
                   screenCaps.style.display = "block";
+                  if (link.querySelectorAll(".screen-marker").length === 0) {
+                    annotateSprite(spriteUrl);
+                  }
                 } else {
                   screenCaps.style.display = "none";
                 }
